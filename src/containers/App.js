b3459b798b25eb7e0fc0c2928e69cf226560e54e
Heavenly Medina <medinah2@tcnj.edu>
2021-11-19 10:10:19 -0500

    Updated to use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,41 @@
 import './App.css';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList.js'
 import SearchBox from '../components/SearchBox'
 import { render } from '@testing-library/react';
 import Scroll from '../components/Scroll.js';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-class App extends React.Component {
-  constructor(){
-      super()
-      this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+function App() {
+  // constructor(){
+  //     super()
+  //     this.state = {
+  //     robots: [],
+  //     searchfield: ''
+  //   }
+  // }
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
+  const [count, setCount] = useState(0);
+  // componentDidMount(){
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then(response => response.json())
+  //   .then(users => this.setState({robots: users}));
+  // }
 
-  componentDidMount(){
+  useEffect(()=>{
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => this.setState({robots: users}));
+    .then(users => setRobots(users));
+    console.log(count);
+  }, [count]) // only run if count changes
+
+  const onSearchChange = (event) =>{
+    setSearchfield(event.target.value);
   }
 
-  onSearchChange = (event) =>{
-    this.setState({searchfield: event.target.value});
-  }
 
-  render(){ 
-    const {robots, searchfield } = this.state;
+    // const {robots, searchfield } = this.state;
     const filteredRobots = robots.filter(robot =>{
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
@@ -37,7 +46,8 @@ class App extends React.Component {
       return (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <button onClick={()=>setCount(count+1)}>Click Me!</button>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filteredRobots} />
@@ -46,7 +56,6 @@ class App extends React.Component {
         </div>
       );
     }
-  }
 }
 
 export default App;
